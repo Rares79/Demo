300a4043ffb4858c7fe238fe445ee431c54b9532
Rares Moldovan <rares.moldovan@iquestgroup.com>
2017-04-19 16:04:59 +0300

    Exercitiu1

---
+++ Ex_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 20;
+            int multiplier = 5;
+
+            int[] myArray = new int[size];
+            
+            for(int i=0;i<size;i++)
+            {
+                myArray[i] = i * multiplier; 
+            }
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
