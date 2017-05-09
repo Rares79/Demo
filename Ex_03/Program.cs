d4be8bcfa5c08caa8e70e02a2196262e38206ff5
Rares Moldovan <rares.moldovan@iquestgroup.com>
2017-05-09 17:23:38 +0300

    tmp

--- Ex_03/Program.cs
+++ Ex_03/Program.cs
@@ -2,61 +2,59 @@
 
 using System;
 
-namespace Ex_03
+namespace Telerik.Array.Ex_02.CompareTwoArraysLexicograpf
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduceti stringul 1: ");
-            string string1 = Console.ReadLine();
+            Console.WriteLine("Insert string number one: ");
+            string firstString = Console.ReadLine();
 
-            Console.WriteLine("Introduceti stringul 2: ");
-            string string2 = Console.ReadLine();
+            Console.WriteLine("Insert string number two: ");
+            string secondString = Console.ReadLine();
 
-            // RV: naming
-            int size1 = string1.Length;
-            int size2 = string2.Length;
-            int minim = Math.Min(size1, size2);
+            int sizeOfFirstString = firstString.Length;
+            int sizeOfSecondString = secondString.Length;
+            int minimumSize = Math.Min(sizeOfFirstString, sizeOfSecondString);
 
             // RV: extrat method
-            for (int i = 0; i < minim; i++)
+            for (int i = 0; i < minimumSize; i++)
             {
                 // RV: if you use continue there is no need of else
-                if (string1[i] == string2[i])
+                if (firstString[i] == secondString[i])
                 {
                     continue;
                 }
                 // RV: why not create a string textToDisplay with the string that you want to print and call
                 // console.writeline only one time
-                // string result = string1[i] < string2[i] ? "A" : "B";
-                else if (string1[i] < string2[i])
+                // string result = firstString[i] < secondString[i] ? "A" : "B";
+                else if (firstString[i] < secondString[i])
                 {
-                    Console.WriteLine(string1 + " este inainte de " + string2);
+                    Console.WriteLine(firstString + " este inainte de " + secondString);
                 }
                 else
                 {
-                    Console.WriteLine(string2 + " este inainte de " + string1);
+                    Console.WriteLine(secondString + " este inainte de " + firstString);
                 }
                 // RV: formating
             }
-            if (string1==string2)
+            if (firstString == secondString)
             {
                 Console.WriteLine("Stringurile sunt identice");
             }
-               else if (size1<size2)
+            else if (sizeOfFirstString < sizeOfSecondString)
             {
-                Console.WriteLine(string1 + " este inainte de " + string2);
-
+                Console.WriteLine(firstString + " este inainte de " + secondString);
             }
-              
-             else
 
-            { 
-                Console.WriteLine(string2 + " este inainte de " + string1);
+            else
+
+            {
+                Console.WriteLine(secondString + " este inainte de " + firstString);
             }
             Console.ReadLine();
 
-            }
         }
     }
+}
---
+++ Telerik.MultiDimArray.Ex_01.FillAndPrint/Program.cs
@@ -0,0 +1,31 @@
+﻿//Write a program that fills and prints a matrix of size(n, n) as shown below: (examples for n = 4)
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telerik.MultiDimArray.Ex_01.FillAndPrint
+{
+    class Program
+    {
+        public static void PrintMatrix(int[,] matrix, int n)
+        {
+            for (int row = 0; row< matrix.GetLength(0); row++)
+            {
+                for (int column = 0;row< matrix.GetLength(1); column++)
+                {
+                    Console.Write("{0,5}", matrix[row, column]);
+
+                }
+                Console.WriteLine(); 
+            }
+
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
