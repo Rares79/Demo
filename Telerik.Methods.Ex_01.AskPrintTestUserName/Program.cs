73765502252f9e72d1769b50e3a01d568d1de4f0
Rares Moldovan <rares.moldovan@iquestgroup.com>
2017-05-11 11:00:14 +0300

    Methods ex 01 done

---
+++ Telerik.Methods.Ex_01.AskPrintTestUserName/Program.cs
@@ -0,0 +1,21 @@
+﻿//Write a method that asks the user for his name and prints “Hello, <name>” (for example, “Hello, Peter!”). Write a program to test this method.
+
+using System;
+
+namespace Telerik.Methods.Ex_01.AskPrintTestUserName
+{
+    class Program
+    {
+        private static void GetUserName()
+        {
+            Console.Write("Please insert your name: ");
+            string userNameToPrint = Console.ReadLine();
+            Console.WriteLine("Hello {0} !", userNameToPrint);
+        }
+        static void Main()
+        {
+            GetUserName();
+            Console.ReadLine();
+        }
+    }
+}
