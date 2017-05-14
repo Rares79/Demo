cde89a7b3845525aea48f4fd5f73190a8d5ff2c6
Rares Moldovan <rares.moldovan@iquestgroup.com>
2017-05-14 12:11:05 +0300

    Write a program to convert decimal numbers to their binary representation.

---
+++ Telerik.Numeral.Ex_01.DecimalToBinary/Program.cs
@@ -0,0 +1,23 @@
+﻿//Write a program to convert decimal numbers to their binary representation.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telerik.Numeral.Ex_01.DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToConvert;
+            Console.WriteLine("Please insert the number for conversion:");
+            numberToConvert = int.Parse(Console.ReadLine());
+            string numberConvertedInBinary = Convert.ToString(numberToConvert, 2);
+            Console.WriteLine(numberConvertedInBinary);
+            Console.ReadLine();
+        }
+    }
+}
