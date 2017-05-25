bcf4e4d52185b4a8448684d39c805c21f34125b2
Rares Moldovan <rares.moldovan@iquestgroup.com>
2017-05-25 14:34:09 +0300

    ex1

--- OOP_Exercise/Telerik.OOP.Ex_01.DefineClasses/Display.cs
+++ OOP_Exercise/Telerik.OOP.Ex_01.DefineClasses/Display.cs
@@ -1,9 +1,10 @@
 ﻿namespace Telerik.OOP.Ex_01.DefineClasses
 {
-    class Display
+    public class Display
     {
         public string Model { get; set; }
         public int HoursIdle { get; set; }
         public int HoursTalk { get; set; }
     }
+
 }
